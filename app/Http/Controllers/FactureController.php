129fac6955f20748e220f766a60301c9c0cc4e13
User <atiadjaber39@gmail.com>
2022-02-24 20:41:20 +0100

    add id facture

--- app/Http/Controllers/FactureController.php
+++ app/Http/Controllers/FactureController.php
@@ -118,7 +118,7 @@ class FactureController extends Model
 
             DB::commit();
 
-            return BaseController::successData($client->montant, "تمت العملية بنجاح");
+            return BaseController::successData(["montant" => $client->montant, "id" => $request->id], "تمت العملية بنجاح");
         } catch (\Exception $e) {
             DB::rollback();
             return response()->json(['message' => 'Error ', 'data' => $e], 500);
